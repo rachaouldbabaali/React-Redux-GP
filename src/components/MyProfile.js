4ae918539570574f11985c9ab420366816c4fd93
rachaouldbabaali <rachaould@gmail.com>
2023-06-27 22:44:35 +0100

    Style the rockets in Myprofile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -9,15 +9,15 @@ const MyProfile = () => {
   const filterRocketName = () => rocketin.filter((rocket) => rocket.reserved);
 
   return (
-    <div className={styles.profileWrapper}>
+    <div className={styles.Wrapper}>
       <div className={styles.missionList}>
-        <h3>My Missions</h3>
+        <h2>My Missions</h2>
 
       </div>
 
-      <div className={styles.rocketList}>
-        <h3>My Rockets</h3>
-        <ul className={styles.listing}>
+      <div className={styles.rocketsList}>
+        <h2>My Rockets</h2>
+        <ul className={styles.ul}>
           {filterRocketName().map((rocket) => (
             <li key={rocket.id} className={styles.list}>
               {rocket.name}
