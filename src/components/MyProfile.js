a26e81f92ec9d534f2eb1166c7db71afcf3b8b8a
rachaouldbabaali <rachaould@gmail.com>
2023-06-27 19:09:35 +0100

    Display rockets is working good

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import styles from '../styles/MyProfile.module.css';
 
-const MyProfile = () => (
+const MyProfile = () => {
+  const rockets = useSelector((state) => state.rocket.rockets);
 
-  <div>
-    Hello
-  </div>
+  const rocketin = [].concat(...rockets);
 
-);
+  const filterRocketName = () => rocketin.filter((rocket) => rocket.reserved);
+
+  return (
+    <div className={styles.profileWrapper}>
+      <div className={styles.missionList}>
+        <h3>My Missions</h3>
+
+      </div>
+
+      <div className={styles.rocketList}>
+        <h3>My Rockets</h3>
+        <ul className={styles.listing}>
+          {filterRocketName().map((rocket) => (
+            <li key={rocket.id} className={styles.list}>
+              {rocket.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    </div>
+  );
+};
 
 export default MyProfile;
