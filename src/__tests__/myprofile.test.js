b8945ecbd3b6f140638844d1bd5bd1fe4670b8b8
rachaouldbabaali <rachaould@gmail.com>
2023-07-01 00:25:46 +0100

    Fix eslint error

--- src/__tests__/myprofile.test.js
+++ src/__tests__/myprofile.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import myStore from '../Redux/store';
+import myStore from '../redux/store';
 import MyProfile from '../components/MyProfile';
 
 it('renders correctly', () => {
